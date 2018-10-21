3c75eb9a3106cc1a01ada1603de8d539163d6d7e
Leonardo Ferreira <mail@leonardoferreira.com.br>
2018-10-21 12:56:04 -0300

    Change BeanUtils to avoid call Getters and Setters (#7)

--- src/main/java/br/com/leonardoferreira/jbacon/util/BeanUtils.java
+++ src/main/java/br/com/leonardoferreira/jbacon/util/BeanUtils.java
@@ -1,43 +1,27 @@
 package br.com.leonardoferreira.jbacon.util;
 
-import br.com.leonardoferreira.jbacon.exception.JBaconInvocationException;
-import org.springframework.util.StringUtils;
-
-import java.beans.PropertyDescriptor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
+import java.beans.FeatureDescriptor;
+import java.util.stream.Stream;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 
 /**
  * Created by lferreira on 6/16/17.
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class BeanUtils extends org.springframework.beans.BeanUtils {
 
-    private BeanUtils() {
-    }
-
     public static void copyPropertiesNotNull(final Object source, final Object target) {
-        final Class<?> targetClass = target.getClass();
-        final PropertyDescriptor[] targetPds = getPropertyDescriptors(targetClass);
-        final List<String> ignoreProperties = new ArrayList<>();
+        BeanWrapper targetWrapper = new BeanWrapperImpl(target);
 
-        try {
-            for (PropertyDescriptor t : targetPds) {
-                Method method = null;
-                try {
-                    method = targetClass.getMethod("get" + StringUtils.capitalize(t.getName()));
-                } catch (NoSuchMethodException e) {
-                    method = targetClass.getMethod("is" + StringUtils.capitalize(t.getName()));
-                }
-                if (method.invoke(target) != null) {
-                    ignoreProperties.add(t.getName());
-                }
-            }
-        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
-            throw new JBaconInvocationException(e);
-        }
+        String[] ignoreProperties = Stream.of(targetWrapper.getPropertyDescriptors())
+                .map(FeatureDescriptor::getName)
+                .filter(it -> targetWrapper.getPropertyValue(it) != null)
+                .toArray(String[]::new);
 
-        copyProperties(source, target, ignoreProperties.toArray(new String[0]));
+        copyProperties(source, target, ignoreProperties);
     }
+
 }
